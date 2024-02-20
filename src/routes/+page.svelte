a2e344f25ca593b9ecb385295c0f291d2d9061b7
Axel BORGET <axel.borget@gmail.com>
2024-02-20 03:04:17 +0100

    En Chantier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import chantier from '$lib/images/en-chantier-1.png';
 </script>
 
 <svelte:head>
@@ -10,49 +11,44 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1> EL CEP </h1>
+	<span class="welcome">
+		<picture>
+			<source srcset={chantier} type="image/webp" />
+			<img src={chantier} alt="Welcome" />
+		</picture>
+	</span>
 </section>
 
 <style>
+
 	section {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
 	}
 
 	h1 {
-		width: 100%;
+		font-family :"Roboto";
+		font-size: 3.75rem;
+		line-height: 1;
+		font-weight: 700;
+		letter-spacing: -0.025em;
 	}
-
 	.welcome {
 		display: block;
 		position: relative;
-		width: 100%;
+		width: 50%;
 		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+		padding: 16px 0 calc(100% * 512 / 512) 0;
+		margin-top:  5rem;
 	}
 
 	.welcome img {
 		position: absolute;
 		width: 100%;
-		height: 100%;
+		height: 50%;
 		top: 0;
 		display: block;
 	}
