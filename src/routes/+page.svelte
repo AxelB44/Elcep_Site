16c3ad3135016be2ba2d90abae54c77d6eb1bf6d
Axel BORGET <axel.borget@gmail.com>
2024-02-20 03:20:22 +0100

    Clean Code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import chantier from '$lib/images/en-chantier-1.png';
 </script>
 
