fb5497707fcf2c00aac59643d3894f04a298ad3b
Axel BORGET <axel.borget@gmail.com>
2024-04-19 16:01:35 +0200

    V0.5.2

--- src/lib/components/Partenaire.svelte
+++ src/lib/components/Partenaire.svelte
@@ -12,5 +12,5 @@
 	<img class="size-60" src={defaultImg} alt="Logo de {Name}" />
 	{/if}
 	
-	<h2 class="m-2 font-rock text-2xl">{Name}</h2>
+	<h2 class="m-2 font-rock text-2xl text-center">{Name}</h2>
 </div>
