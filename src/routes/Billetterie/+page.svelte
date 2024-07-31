d9fb5b73c4e30062ecaf8ee5a6ad89f4aedf525c
Axel BORGET <axel.borget@gmail.com>
2024-07-31 17:53:31 +0200

    Merch V3

--- src/routes/Billetterie/+page.svelte
+++ src/routes/Billetterie/+page.svelte
@@ -120,30 +120,3 @@
 		>
 	</div>
 </section>
-
-<section class="flex flex-col p-12 h-full bg-gray-100">
-	<h1 class="text-center text-5xl font-rock mb-8">Merch</h1>
-	<div class="flex w-full h-full">
-
-		<!--
-		<iframe
-			title="Bouton Merch"
-			id="haWidget"
-			allowtransparency={true}
-			src="https://www.helloasso.com/associations/association-culturelle-les-saltimbanques-de-la-grigonnais/boutiques/vente-de-textiles-el-cep-2024/widget-bouton"
-			style="width: 100%; height: 70px; border: none;"
-		></iframe>
-	-->
-
-		<iframe
-			title="Bouton Merch"
-			id="haWidget"
-			allowtransparency={true}
-			scrolling="yes"
-			src="https://www.helloasso.com/associations/association-culturelle-les-saltimbanques-de-la-grigonnais/boutiques/vente-de-textiles-el-cep-2024/widget"
-			class=" w-full h-[1900px]"
-			height="100%" width="100%"
-		></iframe>
-		
-	</div>
-</section>
